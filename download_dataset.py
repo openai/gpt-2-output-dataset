1e78accdc638d7f57d023e829a78ac3cf67d844c
Aarni Koskela <akx@iki.fi>
2019-06-11 14:34:41 +0300

    Update progress bar less often

--- download_dataset.py
+++ download_dataset.py
@@ -19,9 +19,7 @@ for ds in [
 
         with open(os.path.join(subdir, filename), 'wb') as f:
             file_size = int(r.headers["content-length"])
-            chunk_size = 1000
             with tqdm(ncols=100, desc="Fetching " + filename, total=file_size, unit_scale=True) as pbar:
-                # 1k for chunk_size, since Ethernet packet size is around 1500 bytes
-                for chunk in r.iter_content(chunk_size=chunk_size):
+                for chunk in r.iter_content(chunk_size=4194304):
                     f.write(chunk)
-                    pbar.update(chunk_size)
+                    pbar.update(len(chunk))
