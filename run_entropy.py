6b32ce8fa7616bff6376c03f56753125fcd51e35
Yang Xu <innerfirexy@gmail.com>
2022-11-16 09:50:27 -0800

    run entropy

--- run_entropy.py
+++ run_entropy.py
@@ -1,5 +1,4 @@
 import argparse
-import glob
 import os
 import torch
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
@@ -13,18 +12,31 @@ from baseline import _load_split
 
 def create_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--input', '-i', type=str, default='', help='input file or dir') # The torchtext way of loading data
+    parser.add_argument('--data_dir', '-d', type=str, default='data/', help='data dir') 
+    parser.add_argument('--source', type=str, choices=[
+        'webtext',
+        'small-117M',  'small-117M-k40',
+        'medium-345M', 'medium-345M-k40',
+        'large-762M',  'large-762M-k40',
+        'xl-1542M',    'xl-1542M-k40',
+    ])
+    parser.add_argument('--split', type=str, choices=['train', 'test', 'valid'])
     parser.add_argument('--output', '-o', type=str, default='', help='output file or dir')
-    parser.add_argument('--min_len', type=int, default=2)
-    parser.add_argument('--model', type=str, 
-        choices=['gpt2'], 
-        default='gpt2')
     return parser
 
 def load_model(args):
     model_class = GPT2LMHeadModel
     tokenizer_class = GPT2Tokenizer
-    pretrained_weights = 'gpt2'
+    if args.source == 'webtext':
+        pretrained_weights = 'gpt2'
+    elif args.source.startswith('small'):
+        pretrained_weights = 'gpt2'
+    elif args.source.startswith('medium'):
+        pretrained_weights = 'gpt2-medium'
+    elif args.source.startswith('large'):
+        pretrained_weights = 'gpt2-large'
+    elif args.source.startswith('xl'):
+        pretrained_weights = 'gpt2-xl'
     model = model_class.from_pretrained(pretrained_weights)
     tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
     num_gpus = torch.cuda.device_count()
@@ -32,20 +44,21 @@ def load_model(args):
     return model, tokenizer
 
 @torch.no_grad()
-def process_single(input_file, output_file, model, tokenizer, args):
+def process_single(model, tokenizer, args):
     device = model.device
     criterian = nn.NLLLoss(reduction='none')
-    softmax = nn.Softmax(dim=1)
     log_softmax = nn.LogSoftmax(dim=1)
 
-    # Read lines from input
-    with open(input_file, 'r') as fr, open(output_file, 'w') as fw:
-        for line in tqdm(fr.readlines()):
-            line = line.strip()
+    data = _load_split('data', source=args.source, split=args.split)
+    if args.output:
+        output_file = args.output
+    else:
+        output_file = os.path.join(data, f'{args.source}.{args.split}.nll')
+
+    with open(output_file, 'w') as fw:
+        for line in tqdm(data):
             encoded_input = tokenizer(line, return_tensors='pt').to(device)
             input_ids = encoded_input['input_ids']
-            if input_ids.shape[-1] < args.min_len: # Skip short ones whose length < args.min_len
-                continue
 
             output = model(**encoded_input, labels= input_ids)
             logits = output.logits.to(device)
@@ -55,13 +68,11 @@ def process_single(input_file, output_file, model, tokenizer, args):
             shift_logits = logits[..., :, :-1] # Use the first L-1 tokens to predict the next
             shift_target = target[..., 1:]
 
-            nll_loss = None
-            if args.type == 'nll_loss':
-                nll_loss = criterian(log_softmax(shift_logits), shift_target).squeeze()
-                res = nll_loss.tolist()
+            nll_loss = criterian(log_softmax(shift_logits), shift_target).squeeze()
+            res = nll_loss.tolist()
             if not isinstance(res, list):
                 res = [res]
-            # Write to output
+
             try:
                 res_str = ' '.join(f'{num:.4f}' for num in res)
             except Exception:
@@ -73,15 +84,9 @@ def process_single(input_file, output_file, model, tokenizer, args):
             else:
                 fw.write(f'{res_str}\n')
 
-def main():
-    pass
-
 
 if __name__ == "__main__":
     parser = create_parser()
     args = parser.parse_args()
     model, tokenizer = load_model(args)
-    if os.path.isfile(args.input):
-        process_single(args.input, args.output, model, tokenizer, args)
-    elif os.path.isdir(args.input):
-        process_batch(model, tokenizer, args)
+    process_single(model, tokenizer, args)
