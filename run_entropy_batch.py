80c0b7d1e45f5b9b49707ea9a0339642088c9f06
xy <innerfirexy@gmail.com>
2022-11-16 11:10:26 -0800

    update

--- run_entropy_batch.py
+++ run_entropy_batch.py
@@ -5,6 +5,7 @@ from transformers import GPT2LMHeadModel, GPT2Tokenizer
 from tqdm import tqdm
 import torch.nn as nn
 from einops import rearrange
+from parallelformers import parallelize
 
 from baseline import _load_split
 
@@ -20,6 +21,7 @@ def create_parser():
         'xl-1542M',    'xl-1542M-k40',
     ])
     parser.add_argument('--split', type=str, choices=['train', 'test', 'valid'])
+    parser.add_argument('--batch_size', '-bs', type=int, default=32)
     parser.add_argument('--output', '-o', type=str, default='', help='output file or dir')
     return parser
 
@@ -36,15 +38,23 @@ def load_model(args):
         pretrained_weights = 'gpt2-large'
     elif args.source.startswith('xl'):
         pretrained_weights = 'gpt2-xl'
+
     model = model_class.from_pretrained(pretrained_weights)
     tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
+    tokenizer.pad_token = tokenizer.eos_token
+
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model.to(device)
+    # num_gpus = torch.cuda.device_count()
+    # parallelize(model, num_gpus=num_gpus, fp16=False, verbose='simple')
+
     return model, tokenizer
+
 
 @torch.no_grad()
 def process_single(model, tokenizer, args):
     device = model.device
+    print(f'model is on device: {device}')
     criterian = nn.NLLLoss(reduction='none')
     log_softmax = nn.LogSoftmax(dim=1)
 
@@ -54,33 +64,31 @@ def process_single(model, tokenizer, args):
     else:
         output_file = os.path.join(args.data_dir, f'{args.source}.{args.split}.nll')
 
+    num_batches = round(len(data)/args.batch_size)
     with open(output_file, 'w') as fw:
-        for line in tqdm(data[:32]):
-            encoded_input = tokenizer(line, return_tensors='pt').to(device)
+        for i in tqdm(range(1)):
+            batch = data[i*args.batch_size: (i*args.batch_size+args.batch_size)]
+            if len(batch) == 0:
+                continue
+            
+            encoded_input = tokenizer(batch, return_tensors='pt', padding=True).to(device)
             input_ids = encoded_input['input_ids']
+            mask = encoded_input['attention_mask']
 
-            output = model(**encoded_input, labels= input_ids)
+            output = model(**encoded_input, labels=input_ids)
             logits = output.logits.to(device)
             target = encoded_input['input_ids'].to(device)
 
             logits = rearrange(logits, 'B L V -> B V L') 
             shift_logits = logits[..., :, :-1] # Use the first L-1 tokens to predict the next
             shift_target = target[..., 1:]
+            mask = mask[..., 1:]
 
             nll_loss = criterian(log_softmax(shift_logits), shift_target).squeeze()
-            res = nll_loss.tolist()
-            if not isinstance(res, list):
-                res = [res]
-
-            try:
-                res_str = ' '.join(f'{num:.4f}' for num in res)
-            except Exception:
-                print('line:', line)
-                print('input_ids:', input_ids)
-                print('logits.shape:', logits.shape)
-                print('res:', res)
-                raise
-            else:
+            for i in range(nll_loss.size(0)):
+                out = nll_loss[i,:].squeeze()
+                out_masked = torch.masked_select(out, mask[i,:]>0)
+                res_str = ' '.join(f'{num:.4f}' for num in out_masked)
                 fw.write(f'{res_str}\n')
 
 
