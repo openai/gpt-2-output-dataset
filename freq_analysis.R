693bbe8d0afe901601de3dbdfa92f36985713180
xy <innerfirexy@gmail.com>
2022-11-23 21:48:24 -0800

    play with degen outputs and gpt2.transformer architecture

--- freq_analysis.R
+++ freq_analysis.R
@@ -126,3 +126,24 @@ d_medium_c <- rbindlist(list(d_medium_gpt2, d_medium_medium, d_medium_large, d_m
 p_medium_c <- ggplot(d_medium_c, aes(freq, power)) +
   geom_smooth(aes(linetype = model, fill = model, colour = model))
 ggsave("plot/medium-345M.test.4models.freq_power_1k.pdf", plot=p_medium_c)
+
+
+###
+# degen data
+d_dg0 <- fread("plot/unconditional_gold.model=gpt2.freq_power.csv")
+d_dg0$cat <- "gold"
+d_dg1 <- fread("plot/unconditional_puresampling_large.model=gpt2.freq_power.csv")
+d_dg1$cat <- "puresampling"
+d_dg2 <- fread("plot/unconditional_sampling_t=0.9_large.model=gpt2.freq_power.csv")
+d_dg2$cat <- "sampling_t=0.9"
+d_dg3 <- fread("plot/unconditional_topk_k=40_t=0.7_large.model=gpt2.freq_power.csv")
+d_dg3$cat <- "topk_k=40_t=0.7"
+d_dg4 <- fread("plot/unconditional_topk_k=640_large.model=gpt2.freq_power.csv")
+d_dg4$cat <- "topk_k=640"
+d_dg5 <- fread("plot/unconditional_topp_p=0.95_large.model=gpt2.freq_power.csv")
+d_dg5$cat <- "topp_p=0.95"
+
+d_dg_c <- rbindlist(list(d_dg0, d_dg1, d_dg2, d_dg3, d_dg4, d_dg5))
+p_dg_c <- ggplot(d_dg_c, aes(freq, power)) +
+  geom_smooth(aes(linetype = cat, fill = cat, colour = cat))
+ggsave("plot/unconditional.5catss.freq_power.pdf", plot=p_dg_c)
