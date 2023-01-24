6b0bf8872ef782f5eed65ee6182f222f4ca27d0d
Yang Xu <innerfirexy@gmail.com>
2023-01-24 08:46:05 -0800

    add plots

--- plot_freqpower.R
+++ plot_freqpower.R
@@ -69,24 +69,27 @@ ggsave("plot/medium-345M.test.4models.freq_power_1k.pdf", plot=p_medium_c)
 # medium-345M-k40.test
 d_medium_k40_gpt2 <- fread("plot/medium-345M-k40.test.model=gpt2.freq_power.csv")
 
-
 # large-762M.test
+d_large_gpt2 <- fread("plot/large-762M.test.model=gpt2.freq_power.csv")
 
 # large-762M-k40.test
+d_large_k40_gpt2 <- fread("plot/large-762M-k40.test.model=gpt2.freq_power.csv")
 
 # xl-1542M.test
+d_xl_gpt2 <- fread("plot/xl-1542M.test.model=gpt2.freq_power.csv")
 
 # xl-1542M-k40.test
+d_xl_k40_gpt2 <- fread("plot/xl-1542M-k40.test.model=gpt2.freq_power.csv")
 
 
 ####
 # power~freq plots for different datasets
+####
 d_gpt2_webtext <- d_webtext_gpt2[,.(freq, power)]
 d_gpt2_webtext$data <- "webtext"
 
 d_gpt2_small <- d_small_gpt2[,.(freq, power)]
 d_gpt2_small$data <- "small"
-
 d_gpt2_small40k <- d_small_k40_gpt2[,.(freq, power)]
 d_gpt2_small40k$data <- "small-40k"
 
@@ -95,9 +98,9 @@ p_gpt2_webtext_small <- ggplot(d_gpt2_webtext_small, aes(freq, power)) +
   geom_smooth(aes(linetype = data, fill = data, colour = data))
 ggsave("plot/gpt2_webtext_small_small40k.freq_power.pdf", plot=p_gpt2_webtext_small)
 
+# medium
 d_gpt2_medium <- d_medium_gpt2[,.(freq, power)]
 d_gpt2_medium$data <- "medium"
-
 d_gpt2_medium40k <- d_medium_k40_gpt2[,.(freq, power)]
 d_gpt2_medium40k$data <- "medium-40k"
 
@@ -107,6 +110,42 @@ p_gpt2_wb_sm_me <- ggplot(d_gpt2_wb_sm_me, aes(freq, power)) +
 ggsave("plot/gpt2_webtext_small_medium.freq_power.pdf", plot=p_gpt2_wb_sm_me)
 
 d_gpt2_wb_me_me40k <- rbindlist(list(d_gpt2_webtext, d_gpt2_medium, d_gpt2_medium40k))
-p_gpt2_wb_sm_me <- ggplot(d_gpt2_wb_me_me40k, aes(freq, power)) +
+p_gpt2_wb_me_me40k <- ggplot(d_gpt2_wb_me_me40k, aes(freq, power)) +
   geom_smooth(aes(linetype = data, fill = data, colour = data))
-ggsave("plot/gpt2_webtext_medium_medium40k.freq_power.pdf", plot=p_gpt2_wb_sm_me)
+ggsave("plot/gpt2_webtext_medium_medium40k.freq_power.pdf", plot=p_gpt2_wb_me_me40k)
+
+# large
+d_gpt2_large <- d_large_gpt2[,.(freq, power)]
+d_gpt2_large$data <- "large"
+d_gpt2_large40k <- d_large_k40_gpt2[,.(freq, power)]
+d_gpt2_large40k$data <- "large-40k"
+
+d_gpt2_wb_la_la40k <- rbindlist(list(d_gpt2_webtext, d_gpt2_large, d_gpt2_large40k))
+p_gpt2_wb_la_la40k <- ggplot(d_gpt2_wb_la_la40k, aes(freq, power)) +
+  geom_smooth(aes(linetype = data, fill = data, colour = data))
+ggsave("plot/gpt2_webtext_large_large40k.freq_power.pdf", plot=p_gpt2_wb_la_la40k)
+
+# xl
+d_gpt2_xl <- d_xl_gpt2[,.(freq, power)]
+d_gpt2_xl$data <- "xl"
+d_gpt2_xl40k <- d_xl_k40_gpt2[,.(freq, power)]
+d_gpt2_xl40k$data <- "xl-40k"
+
+d_gpt2_wb_xl_xl40k <- rbindlist(list(d_gpt2_webtext, d_gpt2_xl, d_gpt2_xl40k))
+p_gpt2_wb_xl_xl40k <- ggplot(d_gpt2_wb_xl_xl40k, aes(freq, power)) +
+  geom_smooth(aes(linetype = data, fill = data, colour = data))
+ggsave("plot/gpt2_webtext_xl_xl40k.freq_power.pdf", plot=p_gpt2_wb_xl_xl40k)
+
+# Compare all data wthout k40
+d_gpt2_wb_sm_me_la_xl <- rbindlist(list(d_gpt2_webtext,
+                                       d_gpt2_small, d_gpt2_medium, d_gpt2_large, d_gpt2_xl))
+p_gpt2_wb_sm_me_la_xl <- ggplot(d_gpt2_wb_sm_me_la_xl, aes(freq, power)) +
+  geom_smooth(aes(linetype = data, fill = data, colour = data))
+ggsave("plot/gpt2_webtext_small_medium_large_xl.freq_power.pdf", plot=p_gpt2_wb_sm_me_la_xl)
+
+# Compare all data with k40
+d_gpt2_wb_sm_me_la_xl_k40 <- rbindlist(list(d_gpt2_webtext,
+                                        d_gpt2_small40k, d_gpt2_medium40k, d_gpt2_large40k, d_gpt2_xl40k))
+p_gpt2_wb_sm_me_la_xl_k40 <- ggplot(d_gpt2_wb_sm_me_la_xl_k40, aes(freq, power)) +
+  geom_smooth(aes(linetype = data, fill = data, colour = data))
+ggsave("plot/gpt2_webtext_small_medium_large_xl.k40.freq_power.pdf", plot=p_gpt2_wb_sm_me_la_xl_k40)
