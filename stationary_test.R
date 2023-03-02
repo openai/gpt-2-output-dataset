fb52c90442dc1058c4675f0d453d19f33170447f
xy <innerfirexy@gmail.com>
2023-03-02 13:50:17 -0800

    add stationary test code

---
+++ stationary_test.R
@@ -0,0 +1,23 @@
+require("data.table")
+require("ggplot2")
+require("stringr")
+require("progress") # tqdm for R
+
+require("fpp")
+require("forecast")
+
+
+# Read entropy data
+file_name <- "data/small-117M.test.model=gpt2.nll"
+file_conn <- file(file_name, "r")
+lines <- readLines(file_conn)
+
+pb <- progress_bar$new(total = 100)
+data <- data.table()
+for (i in 1:length(lines)) {
+  entropy = as.numeric(str_split(lines[i], " ")[[1]])
+  seriesID = rep(i, length(entropy))
+  data <- rbindlist(list(data, data.table(seriesID = seriesID, entropy = entropy)))
+  pb$tick()
+}
+
