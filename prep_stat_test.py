a76554df9afc78f9675debddc9b4fc210038cbbc
Yang Xu <innerfirexy@gmail.com>
2023-03-03 14:02:12 -0800

    add code

--- prep_stat_test.py
+++ prep_stat_test.py
@@ -1,3 +1,5 @@
+import itertools
+import os
 import pandas as pd
 from tqdm import tqdm
 
@@ -5,8 +7,20 @@ from tqdm import tqdm
 file_name = "data/small-117M.test.model=gpt2.nll"
 data = {'entropy': [], 'series_id': []}
 with open(file_name, 'r') as f:
-    for i, line in enumerate(f):
-        entropy = list(map(float, line.split(' ')))
+    lines = f.readlines()
+    for i, line in enumerate(tqdm(lines)):
+        entropy = list(map(float, line.split(' ')[:-1]))
         series_id = [i]*len(entropy)
-        data['entropy'] = data['entropy'] + entropy
-        data['series_id'] = data['series_id'] + series_id
+        data['entropy'].append(entropy)
+        data['series_id'].append(series_id)
+
+data['entropy'] = itertools.chain.from_iterable(data['entropy'])
+data['series_id'] = itertools.chain.from_iterable(data['series_id'])
+df = pd.DataFrame.from_dict(data)
+print(df.shape)
+
+output_dir = './stat_test'
+os.makedirs(output_dir, exist_ok=True)
+output_file_name = os.path.basename(file_name) + '.stat.csv'
+output_path = os.path.join(output_dir, output_file_name)
+df.to_csv(output_path, index=False)
