a3d573d662b51a9b297ab0e8cd9077e166458701
xy <innerfirexy@gmail.com>
2023-03-07 14:48:02 -0800

    stat test code

--- stationary_test.R
+++ stationary_test.R
@@ -27,17 +27,35 @@ require("forecast")
 
 # Read new stat.csv data
 dt <- fread("stat_test/small-117M.test.model=gpt2.nll.stat.csv")
-dt.test <- dt[, {
-                 res1 = Box.test(entropy)
-                 res2 = adf.test(entropy)
-                 res3 = kpss.test(entropy)
-                 res4 = pp.test(entropy)
-                 .(boxpval = res1$p.value, adfpval = res2$p.value, kpsspval = res3$p.value, pppval = res4$p.value)
-               }, by = .(series_id)]
-
-
-entropy <- dt[series_id==0]$entropy
-boxpval <- Box.test(entropy)
-adfpval <- adf.test(entropy)
-kpsspval <- kpss.test(entropy)
-pppval <- pp.test(entropy)
+# dt.test <- dt[, {
+#                  res1 = Box.test(entropy)
+#                  res2 = adf.test(entropy)
+#                  res3 = kpss.test(entropy)
+#                  res4 = pp.test(entropy)
+#                  .(boxpval = res1$p.value, adfpval = res2$p.value, kpsspval = res3$p.value, pppval = res4$p.value)
+#                }, by = .(series_id)]
+dt.test <- data.table(series_id = numeric(),
+                      series_len = numeric(),
+                      boxpal = numeric(),
+                      adfpval = numeric(),
+                      kpsspval = numeric(),
+                      pppval = numeric())
+# Suppress warning
+defaultW <- getOption("warn")
+options(warn = -1)
+for (s_id in unique(dt$series_id)) {
+  entropy <- dt[series_id == s_id]$entropy
+  boxpval <- Box.test(entropy)$p.value
+  adfpval <- adf.test(entropy)$p.value
+  kpsspval <- kpss.test(entropy)$p.value
+  pppval <- pp.test(entropy)$p.value
+  tmp <- data.table(series_id = s_id,
+                    series_len = length(entropy),
+                    boxpal = boxpval,
+                    adfpval = adfpval,
+                    kpsspval = kpsspval,
+                    pppval = pppval)
+  dt.test <- rbindlist(list(dt.test, tmp))
+}
+# Restore warning
+options(warn = defaultW)
