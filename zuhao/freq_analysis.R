bb62746b38cb3eaa99a35114d4026cff16673cc8
Zuhao <35811208+mwxely@users.noreply.github.com>
2023-03-18 04:34:18 +0800

    Add files via upload

---
+++ zuhao/freq_analysis.R
@@ -0,0 +1,106 @@
+# install.packages("ggplot2")
+# install.packages("data.table")
+require(ggplot2)
+require(data.table)
+
+# train
+d_webtext <- fread("plot/webtext_freq_power_1k_train.csv")
+d_webtext$source <- "webtext"
+p <- ggplot(d_webtext, aes(freq, power)) + geom_smooth()
+ggsave("plot/webtext_freq_power_1k_smooth_train.pdf", plot=p)
+
+# opt
+d_webtext <- fread("plot/webtext_freq_power_1k_opt.csv")
+d_webtext$source <- "webtext"
+p <- ggplot(d_webtext, aes(freq, power)) + geom_smooth()
+ggsave("plot/webtext_freq_power_1k_smooth_opt.pdf", plot=p)
+
+# valid
+d_webtext <- fread("plot/webtext_freq_power_1k_valid.csv")
+d_webtext$source <- "webtext"
+p <- ggplot(d_webtext, aes(freq, power)) + geom_smooth()
+ggsave("plot/webtext_freq_power_1k_smooth_valid.pdf", plot=p)
+
+# test
+d_webtext <- fread("plot/webtext_freq_power_1k_test.csv")
+d_webtext$source <- "webtext"
+p <- ggplot(d_webtext, aes(freq, power)) + geom_smooth()
+ggsave("plot/webtext_freq_power_1k_smooth_test.pdf", plot=p)
+
+# # small-117M
+# d_small <- fread("plot/small-117M_freq_power_1k.csv")
+# d_small$source <- "small-117M"
+# d_small_k40 <- fread("plot/small-117M-k40_freq_power_1k.csv")
+# d_small_k40$source <- "small-117M-k40"
+
+# p_small <- ggplot(d_small, aes(freq, power)) + geom_smooth()
+# ggsave("plot/small-117M_freq_power_1k_smooth.pdf", plot=p_small)
+
+# # Medium-345M
+# d_medium <- fread("plot/medium-345M_freq_power_1k.csv")
+# d_medium$source <- "medium-345M"
+# d_medium_k40 <- fread("plot/medium-345M-k40_freq_power_1k.csv")
+# d_medium_k40$source <- "medium-345M-k40"
+
+# # Large-762M
+# d_large <- fread("plot/large-762M_freq_power_1k.csv")
+# d_large$source <- "large-762M"
+# d_large_k40 <- fread("plot/large-762M-k40_freq_power_1k.csv")
+# d_large_k40$source <- "large-762M-k40"
+
+# # xl-1542M
+# d_xl <- fread("plot/xl-1542M_freq_power_1k.csv")
+# d_xl$source <- "xl-1542M"
+# d_xl_k40 <- fread("plot/xl-1542M-k40_freq_power_1k.csv")
+# d_xl_k40$source <- "xl-1542M-k40"
+
+
+# # All combined
+# dc <- rbindlist(list(d_webtext, d_small, d_medium, d_large, d_xl))
+# pc <- ggplot(dc, aes(freq, power)) + geom_smooth(aes(linetype = source, fill = source, colour = source))
+# ggsave("plot/webtext_all_freq_power_1k_smooth.pdf", plot=pc)
+
+# dc_k40 <- rbindlist(list(d_webtext, d_small_k40, d_medium_k40, d_large_k40, d_xl_k40))
+# pc_k40 <- ggplot(dc_k40, aes(freq, power)) + geom_smooth(aes(linetype = source, fill = source, colour = source))
+# ggsave("plot/webtext_all-k40_freq_power_1k_smooth.pdf", plot=pc_k40)
+
+
+# # Small and small-k40
+# d_small_c <- rbindlist(list(d_webtext, d_small, d_small_k40))
+# p_small_c <- ggplot(d_small_c, aes(freq, power)) + geom_smooth(aes(linetype = source, fill = source, colour = source))
+# ggsave("plot/webtext_small_small-k40_freq_power_1k_smooth.pdf", plot=p_small_c)
+
+# d_medium_c <- rbindlist(list(d_webtext, d_medium, d_medium_k40))
+# p_medium_c <- ggplot(d_medium_c, aes(freq, power)) + geom_smooth(aes(linetype = source, fill = source, colour = source))
+# ggsave("plot/webtext_medium_medium-k40_freq_power_1k_smooth.pdf", plot=p_medium_c)
+
+# d_large_c <- rbindlist(list(d_webtext, d_large, d_large_k40))
+# p_large_c <- ggplot(d_large_c, aes(freq, power)) + geom_smooth(aes(linetype = source, fill = source, colour = source))
+# ggsave("plot/webtext_large_large-k40_freq_power_1k_smooth.pdf", plot=p_large_c)
+
+# d_xl_c <- rbindlist(list(d_webtext, d_xl, d_xl_k40))
+# p_xl_c <- ggplot(d_xl_c, aes(freq, power)) + geom_smooth(aes(linetype = source, fill = source, colour = source))
+# ggsave("plot/webtext_xl_xl-k40_freq_power_1k_smooth.pdf", plot=p_xl_c)
+
+
+
+
+# ###
+# # degen data
+# d_dg0 <- fread("plot/unconditional_gold.model=gpt2.freq_power.csv")
+# d_dg0$cat <- "gold"
+# d_dg1 <- fread("plot/unconditional_puresampling_large.model=gpt2.freq_power.csv")
+# d_dg1$cat <- "puresampling"
+# d_dg2 <- fread("plot/unconditional_sampling_t=0.9_large.model=gpt2.freq_power.csv")
+# d_dg2$cat <- "sampling_t=0.9"
+# d_dg3 <- fread("plot/unconditional_topk_k=40_t=0.7_large.model=gpt2.freq_power.csv")
+# d_dg3$cat <- "topk_k=40_t=0.7"
+# d_dg4 <- fread("plot/unconditional_topk_k=640_large.model=gpt2.freq_power.csv")
+# d_dg4$cat <- "topk_k=640"
+# d_dg5 <- fread("plot/unconditional_topp_p=0.95_large.model=gpt2.freq_power.csv")
+# d_dg5$cat <- "topp_p=0.95"
+
+# d_dg_c <- rbindlist(list(d_dg0, d_dg1, d_dg2, d_dg3, d_dg4, d_dg5))
+# p_dg_c <- ggplot(d_dg_c, aes(freq, power)) +
+#   geom_smooth(aes(linetype = cat, fill = cat, colour = cat))
+# ggsave("plot/unconditional.5catss.freq_power.pdf", plot=p_dg_c)
---
+++ zuhao/run_entropy.py
@@ -0,0 +1,100 @@
+import argparse
+import os
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from tqdm import tqdm
+import torch.nn as nn
+from einops import rearrange
+
+from baseline import _load_split
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', '-d', type=str, default='data/', help='data dir') 
+    parser.add_argument('--source', type=str, choices=[
+        'webtext',
+        'small-117M',  'small-117M-k40',
+        'medium-345M', 'medium-345M-k40',
+        'large-762M',  'large-762M-k40',
+        'xl-1542M',    'xl-1542M-k40',
+    ])
+    parser.add_argument('--split', type=str, choices=['train', 'test', 'valid'])
+    parser.add_argument('--output', '-o', type=str, default='', help='output file or dir')
+    parser.add_argument('--model', type=str, default='', choices=['gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'], help='if specified, this model will be used for estimating the NLL in replace of the default models')
+    return parser
+
+def load_model(args):
+    model_class = GPT2LMHeadModel
+    tokenizer_class = GPT2Tokenizer
+    if args.source == 'webtext':
+        pretrained_weights = 'gpt2'
+    elif args.source.startswith('small'):
+        pretrained_weights = 'gpt2'
+    elif args.source.startswith('medium'):
+        pretrained_weights = 'gpt2-medium'
+    elif args.source.startswith('large'):
+        pretrained_weights = 'gpt2-large'
+    elif args.source.startswith('xl'):
+        pretrained_weights = 'gpt2-xl'
+    
+    # overwrite with --model
+    if args.model:
+        pretrained_weights = args.model
+
+    model = model_class.from_pretrained(pretrained_weights)
+    tokenizer = tokenizer_class.from_pretrained(pretrained_weights)
+    tokenizer.pad_token = tokenizer.eos_token
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
+
+    return model, tokenizer
+
+@torch.no_grad()
+def process_single(model, tokenizer, args):
+    device = model.device
+    criterian = nn.NLLLoss(reduction='none')
+    log_softmax = nn.LogSoftmax(dim=1)
+
+    data = _load_split('data', source=args.source, split=args.split)
+    if args.output:
+        output_file = args.output
+    else:
+        output_file = os.path.join(args.data_dir, f'{args.source}.{args.split}.model={args.model}.nll')
+
+    with open(output_file, 'w') as fw:
+        print("Computing entropy......")
+        for line in tqdm(data, total=len(data)):
+            encoded_input = tokenizer(line, return_tensors='pt').to(device)
+            input_ids = encoded_input['input_ids']
+
+            output = model(**encoded_input, labels= input_ids)
+            logits = output.logits.to(device)
+            target = encoded_input['input_ids'].to(device)
+
+            logits = rearrange(logits, 'B L V -> B V L') 
+            shift_logits = logits[..., :, :-1] # Use the first L-1 tokens to predict the next
+            shift_target = target[..., 1:]
+
+            nll_loss = criterian(log_softmax(shift_logits), shift_target).squeeze()
+            res = nll_loss.tolist()
+            if not isinstance(res, list):
+                res = [res]
+
+            try:
+                res_str = ' '.join(f'{num:.4f}' for num in res)
+            except Exception:
+                print('line:', line)
+                print('input_ids:', input_ids)
+                print('logits.shape:', logits.shape)
+                print('res:', res)
+                raise
+            else:
+                fw.write(f'{res_str}\n')
+
+
+if __name__ == "__main__":
+    parser = create_parser()
+    args = parser.parse_args()
+    model, tokenizer = load_model(args)
+    process_single(model, tokenizer, args)
