9e6a48227f4102d56f59a904aa03b3ac0b898ec6
kyaasky <149972831+kyaasky@users.noreply.github.com>
2023-11-11 09:06:48 -0500

    full rewrite + improved.

--- download_dataset.py
+++ download_dataset.py
@@ -1,9 +1,4 @@
-import os
-import sys
 import requests
-from tqdm import tqdm
-
-subdir = 'data'import requests
 import json
 import os 
 from dataclasses import dataclass, asdict
@@ -103,27 +98,3 @@ class GPTData:
 
 gpt_data = GPTData(target_examples=None)  # Can be left as None, or you can provide an integer based on how much data you want.
 gpt_data.download_and_save_data(data_size_fn='webtext', split_fn='train')
-
-if not os.path.exists(subdir):
-    os.makedirs(subdir)
-subdir = subdir.replace('\\','/') # needed for Windows
-
-for ds in [
-    'webtext',
-    'small-117M',  'small-117M-k40',
-    'medium-345M', 'medium-345M-k40',
-    'large-762M',  'large-762M-k40',
-    'xl-1542M',    'xl-1542M-k40',
-]:
-    for split in ['train', 'valid', 'test']:
-        filename = ds + "." + split + '.jsonl'
-        r = requests.get("https://openaipublic.azureedge.net/gpt-2/output-dataset/v1/" + filename, stream=True)
-
-        with open(os.path.join(subdir, filename), 'wb') as f:
-            file_size = int(r.headers["content-length"])
-            chunk_size = 1000
-            with tqdm(ncols=100, desc="Fetching " + filename, total=file_size, unit_scale=True) as pbar:
-                # 1k for chunk_size, since Ethernet packet size is around 1500 bytes
-                for chunk in r.iter_content(chunk_size=chunk_size):
-                    f.write(chunk)
-                    pbar.update(chunk_size)
